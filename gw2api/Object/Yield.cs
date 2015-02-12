8316812b5c043e5bd55afbf8187bdb4f582e6dba
Yong Wen Chua <yongwen@lshift.net>
2015-02-12 12:16:30 +0000

    Add yield object

---
+++ gw2api/Object/Yield.cs
@@ -0,0 +1,36 @@
+﻿namespace gw2api.Object
+{
+    public class Yield
+    {
+        public int Upper { get; set; }
+        public int Lower { get; set; }
+        public int? Average { get; set; } // Can be unknown, then it's null
+
+        // Some useful data, sourced from GW2 Wiki
+        public static Yield FineMaterialsPromotionYielld = new Yield()
+        {
+            Lower = 7,
+            Upper = 40,
+            Average = 18
+        };
+
+        public static Yield FineMaterialTier6PromotionYield = new Yield()
+        {
+            Lower = 5,
+            Upper = 12,
+            Average = 6
+        };
+
+        public static Yield DustPromotionYield = new Yield()
+        {
+            Lower = 40,
+            Upper = 200
+        };
+
+        public static Yield CrystallineDustPromotionYield = new Yield()
+        {
+            Lower = 6,
+            Upper = 40
+        };
+    }
+}
