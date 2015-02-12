aaace25243cfe7cde24062dcec64e2bfba7accdd
Yong Wen Chua <yongwen@lshift.net>
2015-02-12 17:33:21 +0000

    Some basic loading bar trickery

--- PromotionViabilityWpf/MainWindow.xaml.cs
+++ PromotionViabilityWpf/MainWindow.xaml.cs
@@ -1,15 +1,38 @@
-﻿using MahApps.Metro.Controls;
+﻿using GW2NET.Commerce;
+using GW2NET.Items;
+using PromotionViabilityWpf.ViewModel;
+using ReactiveUI;
 
 namespace PromotionViabilityWpf
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow
+    public partial class MainWindow : IViewFor<MainWindowViewModel>
     {
+        private MainWindowViewModel MainWindowViewModel;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            MainWindowViewModel = new MainWindowViewModel();
+            MainWindowViewModel.Load<int, Item>(Promotions.FineMaterialsTier6Promotions);
+            MainWindowViewModel.Load<int, AggregateListing>(Promotions.FineMaterialsTier6Promotions);
+
+            this.OneWayBind(ViewModel, x => x.IsLoading, x => x.LoadingIndicator.Visibility);
+        }
+
+        object IViewFor.ViewModel
+        {
+            get { return ViewModel; }
+            set { ViewModel = value as MainWindowViewModel; }
+        }
+
+        public MainWindowViewModel ViewModel
+        {
+            get { return MainWindowViewModel; }
+            set { MainWindowViewModel = value; }
         }
     }
 }
---
+++ PromotionViabilityWpf/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using gw2api.Request;
+using ReactiveUI;
+
+namespace PromotionViabilityWpf.ViewModel
+{
+    public class MainWindowViewModel : ReactiveObject
+    {
+        private ReactiveList<Task> ActiveTasks;
+
+        private readonly ObservableAsPropertyHelper<Boolean> loading; 
+        public Boolean IsLoading
+        {
+            get { return loading.Value; }
+        }
+
+        internal MainWindowViewModel()
+        {
+            ActiveTasks = new ReactiveList<Task>();
+
+            this.WhenAnyValue(x => x.ActiveTasks.Count)
+                .Select(x => x != 0)
+                .ToProperty(this, x => x.IsLoading, out loading);
+        }
+
+        public async void Load<TKey, TValue>(IEnumerable<IBundlelable<TKey, TValue>> bundlelable)
+        {
+            var command = ReactiveCommand.CreateAsyncTask(async _ =>
+            {
+                var task = Bundler.BundleAndSet(bundlelable);
+                ActiveTasks.Add(task);
+                await task;
+
+                ActiveTasks.Remove(task);;
+            });
+            await command.ExecuteAsync();
+        }
+
+    }
+}
