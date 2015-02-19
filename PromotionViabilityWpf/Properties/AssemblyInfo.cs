493cb5a4c2f10db38da2fbd5db66bdee9673eab5
Yong Wen Chua <yongwen@lshift.net>
2015-02-19 13:21:17 +0000

    Update some assembly data

--- PromotionViabilityWpf/Properties/AssemblyInfo.cs
+++ PromotionViabilityWpf/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PromotionViabilityWpf")]
+[assembly: AssemblyTitle("Guild Wars 2 Promotion Viability Tool")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PromotionViabilityWpf")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("Guild Wars 2 Promotion Viability Tool")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
